f1287225dd809bada2768fae8dd09be53d0c630c
ianevictoria <ianevictoria.tec@gmail.com>
2023-05-03 23:51:35 -0400

    Fix: Modal Headers

--- src/FinancasNaMaoMVC/FinancasNaMaoMVC/wwwroot/js/headerModal.js
+++ src/FinancasNaMaoMVC/FinancasNaMaoMVC/wwwroot/js/headerModal.js
@@ -2,6 +2,59 @@ const openModalCadastroButton = document.querySelector(".open-modal-Cadastro");
 const modalRegister = document.querySelector("#modalCadastro");
 const fadeRegister = document.querySelector("#fadeCadastro");
 
+const openModalLoginButton = document.querySelector(".open-modal-login");
+const closeModalButton = document.querySelector("#close-modal");
+const modalLogin = document.querySelector("#modalLogin");
+const fadeLogin = document.querySelector("#fadeLogin");
+
+const openModalPasswordButton = document.querySelector(".open-modal-password");
+const modalPassword = document.querySelector("#modalPassword");
+const fadePassword = document.querySelector("#fadePassword");
+
+const backBtn = document.querySelector(".backBtn")
+const registerModalBtn = document.querySelector(".registerModalBtn")
+
+const toggleModalPassword = () => {
+  modalPassword.classList.toggle("hide");
+  fadePassword.classList.toggle("hide");
+};
+
+openModalPasswordButton.addEventListener("click", ()=>{
+  toggleModalLogin()
+  toggleModalPassword()
+})
+
+fadePassword.addEventListener("click", ()=>{
+  toggleModalPassword()
+})
+
+backBtn.addEventListener("click", ()=>{
+  toggleModalPassword()
+  toggleModalLogin()
+
+})
+
+registerModalBtn.addEventListener("click", ()=>{
+  toggleModalLogin()
+  toggleModalRegister()
+})
+
+
+
+const toggleModalLogin = () => {
+  modalLogin.classList.toggle("hide");
+  fadeLogin.classList.toggle("hide");
+};
+
+[openModalLoginButton, fadeLogin].forEach((el) => {
+  el.addEventListener("click", () => toggleModalLogin());
+});
+
+fadeLogin.addEventListener("click", ()=>{
+  modalLogin.classList.add("hide");
+  fadeLogin.classList.add("hide");
+})
+
 
 
     const toggleModalRegister = () => {
